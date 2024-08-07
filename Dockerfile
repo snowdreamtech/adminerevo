178f00d0e70305baffb0d44cb2c2bf899532a502
snowdream <sn0wdr1am@qq.com>
2024-08-07 21:15:48 +0800

    feat: plugins

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,46 @@ FROM snowdreamtech/php:8.1.29
 LABEL maintainer="snowdream <sn0wdr1am@qq.com>"
 
 ENV ADMINER_VERSION=4.8.1 \
-    ADMINER_DESIGN='default'
+    ADMINER_DESIGN='default' \
+    ADMINER_PATH=/usr/share/webapps/adminer \
+    ADMINER_PLUGINS_PATH=/usr/share/webapps/adminer/plugins
 
-RUN mkdir -p /usr/share/webapps/adminer \
-    && wget https://github.com/vrana/adminer/releases/download/v${ADMINER_VERSION}/adminer-${ADMINER_VERSION}.php -O /usr/share/webapps/adminer/index.php \
-    && wget https://github.com/vrana/adminer/releases/download/v${ADMINER_VERSION}/editor-${ADMINER_VERSION}.php -O /usr/share/webapps/adminer/editor.php
+RUN mkdir -p ${ADMINER_PATH} \
+    && mkdir -p ${ADMINER_PLUGINS_PATH} \
+    && wget https://github.com/vrana/adminer/releases/download/v${ADMINER_VERSION}/adminer-${ADMINER_VERSION}.php -O ${ADMINER_PATH}/adminer.php \
+    && wget https://raw.githubusercontent.com/vrana/adminer/master/plugins/plugin.php -O ${ADMINER_PLUGINS_PATH}/plugin.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/database-hide.php -O ${ADMINER_PLUGINS_PATH}/database-hide.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/designs.php -O ${ADMINER_PLUGINS_PATH}/designs.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/dump-alter.php -O ${ADMINER_PLUGINS_PATH}/dump-alter.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/dump-bz2.php -O ${ADMINER_PLUGINS_PATH}/dump-bz2.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/dump-date.php -O ${ADMINER_PLUGINS_PATH}/dump-date.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/dump-json.php -O ${ADMINER_PLUGINS_PATH}/dump-json.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/dump-xml.php -O ${ADMINER_PLUGINS_PATH}/dump-xml.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/dump-zip.php -O ${ADMINER_PLUGINS_PATH}/dump-zip.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/edit-calendar.php -O ${ADMINER_PLUGINS_PATH}/edit-calendar.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/edit-foreign.php -O ${ADMINER_PLUGINS_PATH}/edit-foreign.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/edit-textarea.php -O ${ADMINER_PLUGINS_PATH}/edit-textarea.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/enum-option.php -O ${ADMINER_PLUGINS_PATH}/enum-option.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/enum-types.php -O ${ADMINER_PLUGINS_PATH}/enum-types.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/file-upload.php -O ${ADMINER_PLUGINS_PATH}/file-upload.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/foreign-system.php -O ${ADMINER_PLUGINS_PATH}/foreign-system.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/frames.php -O ${ADMINER_PLUGINS_PATH}/frames.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/json-column.php -O ${ADMINER_PLUGINS_PATH}/json-column.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/login-otp.php -O ${ADMINER_PLUGINS_PATH}/login-otp.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/login-servers.php -O ${ADMINER_PLUGINS_PATH}/login-servers.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/login-password-less.php -O ${ADMINER_PLUGINS_PATH}/login-password-less.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/login-ssl.php -O ${ADMINER_PLUGINS_PATH}/login-ssl.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/login-table.php -O ${ADMINER_PLUGINS_PATH}/login-table.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/master-slave.php -O ${ADMINER_PLUGINS_PATH}/master-slave.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/pretty-json-column.php -O ${ADMINER_PLUGINS_PATH}/pretty-json-column.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/slugify.php -O ${ADMINER_PLUGINS_PATH}/slugify.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/sql-log.php -O ${ADMINER_PLUGINS_PATH}/sql-log.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/struct-comments.php -O ${ADMINER_PLUGINS_PATH}/struct-comments.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/tables-filter.php -O ${ADMINER_PLUGINS_PATH}/tables-filter.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/tinymce.php -O ${ADMINER_PLUGINS_PATH}/tinymce.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/translation.php -O ${ADMINER_PLUGINS_PATH}/translation.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/version-noverify.php -O ${ADMINER_PLUGINS_PATH}/version-noverify.php \
+    && wget https://raw.github.com/vrana/adminer/master/plugins/wymeditor.php -O ${ADMINER_PLUGINS_PATH}/wymeditor.php 
 
 
 EXPOSE 80
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,11 +9,76 @@ ADMINER_DESIGNS_PATH=${ADMINER_PATH}/designs
 if [ ${ADMINER_DESIGN} == 'default' ]; then {
     rm ${ADMINER_DESIGN_FILE}
 } else {
-    if [ -f ${ADMINER_DESIGNS_PATH}/${ADMINER_DESIGN}/adminer.css ];then
+    if [ -f ${ADMINER_DESIGNS_PATH}/${ADMINER_DESIGN}/adminer.css ]; then
         cp ${ADMINER_DESIGNS_PATH}/${ADMINER_DESIGN}/adminer.css ${ADMINER_PATH}
     fi
 }
 fi
+
+# index.php
+cat >${ADMINER_PATH}/index.php <<EOF
+<?php
+function adminer_object() {
+    // required to run any plugin
+    include_once "./plugins/plugin.php";
+    
+    // autoloader
+    foreach (glob("plugins/*.php") as $filename) {
+        include_once "./$filename";
+    }
+    
+    // enable extra drivers just by including them
+    //~ include "./plugins/drivers/simpledb.php";
+    
+    $plugins = array(
+        // specify enabled plugins here
+        new AdminerDatabaseHide(),    // https://raw.githubusercontent.com/vrana/adminer/master/plugins/database-hide.php
+        new AdminerDesigns(),         // https://raw.githubusercontent.com/vrana/adminer/master/plugins/designs.php
+        new AdminerDumpAlter(),       // https://raw.githubusercontent.com/vrana/adminer/master/plugins/dump-alter.php
+        new AdminerDumpBz2(),         // https://raw.githubusercontent.com/vrana/adminer/master/plugins/dump-bz2.php
+        new AdminerDumpDate(),        // https://raw.githubusercontent.com/vrana/adminer/master/plugins/dump-date.php
+        new AdminerDumpJson(),        // https://raw.githubusercontent.com/vrana/adminer/master/plugins/dump-json.php
+        new AdminerDumpXml(),         // https://raw.githubusercontent.com/vrana/adminer/master/plugins/dump-xml.php
+        new AdminerDumpZip(),         // https://raw.githubusercontent.com/vrana/adminer/master/plugins/dump-zip.php
+        new AdminerEditCalendar(),    // https://raw.githubusercontent.com/vrana/adminer/master/plugins/edit-calendar.php     
+        new AdminerEditForeign(),     // https://raw.githubusercontent.com/vrana/adminer/master/plugins/edit-foreign.php    
+        new AdminerEditTextarea(),    // https://raw.githubusercontent.com/vrana/adminer/master/plugins/edit-textarea.php    
+        new AdminerEnumOption(),      // https://raw.githubusercontent.com/vrana/adminer/master/plugins/enum-option.php  
+        new AdminerEnumTypes(),       // https://raw.githubusercontent.com/vrana/adminer/master/plugins/enum-types.php   
+        new AdminerFileUpload("data/"),//https://raw.githubusercontent.com/vrana/adminer/master/plugins/file-upload.php
+        new AdminerForeignSystem(),   // https://raw.githubusercontent.com/vrana/adminer/master/plugins/foreign-system.php     
+        //new AdminerFrames(),        // https://raw.githubusercontent.com/vrana/adminer/master/plugins/frames.php    
+        new AdminerJsonColumn(),      // https://raw.githubusercontent.com/vrana/adminer/master/plugins/json-column.php  
+        new AdminerLoginOtp(),        // https://raw.githubusercontent.com/vrana/adminer/master/plugins/login-otp.php
+        new AdminerLoginServers(),    // https://raw.githubusercontent.com/vrana/adminer/master/plugins/login-servers.php     
+        new AdminerLoginPasswordLess(),// https://raw.githubusercontent.com/vrana/adminer/master/plugins/login-password-less.php         
+        new AdminerLoginSsl(),        // https://raw.githubusercontent.com/vrana/adminer/master/plugins/login-ssl.php
+        new AdminerLoginTable(),      // https://raw.githubusercontent.com/vrana/adminer/master/plugins/login-table.php
+        new AdminerMasterSlave(),     // https://raw.githubusercontent.com/vrana/adminer/master/plugins/master-slave.php
+        new AdminerPrettyJsonColumn(),// https://raw.githubusercontent.com/vrana/adminer/master/plugins/pretty-json-column.php
+        new AdminerSlugify(),         // https://raw.githubusercontent.com/vrana/adminer/master/plugins/slugify.php
+        new AdminerSqlLog(),          // https://raw.githubusercontent.com/vrana/adminer/master/plugins/sql-log.php
+        new AdminerStructComments(),  // https://raw.githubusercontent.com/vrana/adminer/master/plugins/struct-comments.php
+        new AdminerTablesFilter(),    // https://raw.githubusercontent.com/vrana/adminer/master/plugins/tables-filter.php
+        new AdminerTinymce(),         // https://raw.githubusercontent.com/vrana/adminer/master/plugins/tinymce.php
+        new AdminerTranslation(),     // https://raw.githubusercontent.com/vrana/adminer/master/plugins/translation.php
+        new AdminerVersionNoverify(), // https://raw.githubusercontent.com/vrana/adminer/master/plugins/version-noverify.php
+        new AdminerWymeditor(),       // https://raw.githubusercontent.com/vrana/adminer/master/plugins/wymeditor.php
+    );
+    
+    /* It is possible to combine customization and plugins:
+    class AdminerCustomization extends AdminerPlugin {
+    }
+    return new AdminerCustomization($plugins);
+    */
+    
+    return new AdminerPlugin($plugins);
+}
+
+// include original Adminer or Adminer Editor
+include "./adminer.php";
+?>
+EOF
 
 # exec commands
 exec "$@"
